6f6fadf184785b4748c6e17af16f33d9ea4373f7
Diego Gonzalez <gonzalezbusnego@gmail.com>
2023-06-19 09:00:46 -0400

    adjust for support Btn

--- src/app/components/pure/support.tsx
+++ src/app/components/pure/support.tsx
@@ -2,7 +2,7 @@ import { SupportIcon } from '@/app/icons/supportSvg';
 
 export const SupportBtn = () => {
   return (
-    <button className="absolute bottom-10 right-10 bg-white text-blue-700 hover:border-blue-700 font-bold py-2 px-2 rounded-full">
+    <button className="border sticky bottom-6 left-6 bg-white text-blue-700 hover:border-blue-700 font-bold py-2 px-2 rounded-full">
       <div className="flex gap-4 items-center text-2xl pr-4">
         <SupportIcon /> Support
       </div>
